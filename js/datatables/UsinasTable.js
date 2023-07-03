58ddaf56035c3b074314418488c5b858f30bc145
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2023-07-03 12:57:27 -0300

    Refactoring to decrease code duplicated and fixing the ajax request type

--- js/datatables/UsinasTable.js
+++ js/datatables/UsinasTable.js
@@ -1,4 +1,5 @@
 import { PowerPlantsType } from "../usinas/PowerPlantsType.js";
+import { URLBuilder } from "../url/URLBuilder.js";
 
 const usinasTable = () => {
     const defaultSqlURL = 'https://dadosabertos.aneel.gov.br/api/3/action/datastore_search_sql';
@@ -18,17 +19,14 @@ const usinasTable = () => {
        comercialTable(parameters);
     }
 
-    const testeAndComercialTable = (parameters) => {
+    const buildDatatable = (url) => {
         $('#table_usinas_operacao').DataTable({
             serverSide: false,
             bDestroy: true,
             ajax: {
-                url: defaultSqlURL,
-                type: 'POST',
+                url: url,
+                type: 'GET',
                 dataType: "jsonp",
-                data: {
-                    sql: `SELECT * from \"${defaultTable}\" WHERE (\"DatLiberOpComerRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\') OR (\"DatLiberOpTesteRealizado\" BETWEEN \'${parameters.publishFrom}\' AND \'${parameters.publishTo}\')`
-                },
                 dataSrc: response => {
                     return response.result.records;
                 }
@@ -50,68 +48,35 @@ const usinasTable = () => {
         });
     }
 
+    const testeAndComercialTable = (parameters) => {
+        const sqlParameters = {
+            sql: `SELECT * from \"${defaultTable}\" WHERE (\"DatLiberOpComerRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\') OR (\"DatLiberOpTesteRealizado\" BETWEEN \'${parameters.publishFrom}\' AND \'${parameters.publishTo}\')`
+
+        };
+
+        const url = URLBuilder.buildUrl(defaultSqlURL, sqlParameters);
+
+        buildDatatable(url);        
+    }
+
     const testeTable = (parameters) => {
-        $('#table_usinas_operacao').DataTable({
-            serverSide: false,
-            bDestroy: true,
-            ajax: {
-                url: 'https://dadosabertos.aneel.gov.br/api/3/action/datastore_search_sql',
-                type: 'POST',
-                dataType: "jsonp",
-                data: {
-                    sql: `SELECT * from \"75419902-c692-498b-a6ef-85f6d4beb5b2\" WHERE \"DatLiberOpTesteRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\' `
-                },
-                dataSrc: response => {
-                    return response.result.records;
-                }
-            },
-            columns: [
-                { data: 'NomUsina' },
-                {
-                    data: 'SigTipoGeracao',
-                    render: data => {
-                        return PowerPlantsType[data];
-                    }
-                },
-                { data: 'DscComercializacaoEnergia' },
-                { data: 'MdaPotenciaLiberadaComercial' },
-                { data: 'SigUFUsina' },
-                { data: 'DatLiberOpTesteRealizado' },
-                { data: 'DatLiberOpComerRealizado' }
-            ]
-        });
+        const sqlParameters = {
+            sql: `SELECT * from \"75419902-c692-498b-a6ef-85f6d4beb5b2\" WHERE \"DatLiberOpTesteRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\' `
+        };
+
+        const url = URLBuilder.buildUrl(defaultSqlURL, sqlParameters);
+
+        buildDatatable(url);
     };
 
     const comercialTable = (parameters) => {
-        $('#table_usinas_operacao').DataTable({
-            serverSide: false,
-            bDestroy: true,
-            ajax: {
-                url: 'https://dadosabertos.aneel.gov.br/api/3/action/datastore_search_sql',
-                type: 'POST',
-                dataType: "jsonp",
-                data: {
-                    sql: `SELECT * from \"75419902-c692-498b-a6ef-85f6d4beb5b2\" WHERE \"DatLiberOpComerRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\' `
-                },
-                dataSrc: response => {
-                    return response.result.records;
-                }
-            },
-            columns: [
-                { data: 'NomUsina' },
-                {
-                    data: 'SigTipoGeracao',
-                    render: data => {
-                        return PowerPlantsType[data];
-                    }
-                },
-                { data: 'DscComercializacaoEnergia' },
-                { data: 'MdaPotenciaLiberadaComercial' },
-                { data: 'SigUFUsina' },
-                { data: 'DatLiberOpTesteRealizado' },
-                { data: 'DatLiberOpComerRealizado' }
-            ]
-        });
+        const sqlParameters = {
+            sql: `SELECT * from \"75419902-c692-498b-a6ef-85f6d4beb5b2\" WHERE \"DatLiberOpComerRealizado\" BETWEEN \'${parameters.publishFrom}\' AND  \'${parameters.publishTo}\' `
+        };
+
+        const url = URLBuilder.buildUrl(defaultSqlURL, sqlParameters);
+
+        buildDatatable(url);        
     }
 
     return {
